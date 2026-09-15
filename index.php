2832b29e7f91a2c9f040d8e6297a76dea99a62fa
rahulmoi463-spec <rahulmoi463@gmail.com>
2026-09-15 22:08:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -279,9 +279,10 @@ if (isset($_SESSION['user_id'])) {
         <!-- Tutorial Video Box -->
 <div class="container text-center my-4" style="position: relative; z-index: 2;">
     <div class="video-box-card">
-        <h4 class="video-title">🎥 প্যানেল কিভাবে ব্যবহার করবেন দেখুন (Full Tutorial)</h4>
+        <h4 class="video-title">🎥 (Full Tutorial)</h4>
         <div class="ratio ratio-16x9 video-container">
-            <iframe src="https://www.youtube.com/embed/YOUR_VIDEO_ID_HERE" title="Bong Boost Tutorial" allowfullscreen></iframe>
+            <iframe src="https://www.youtube.com/embed/0gq8C40Wn20" title="Bong Boost Tutorial" allowfullscreen></iframe>
+            
         </div>
     </div>
 </div>
